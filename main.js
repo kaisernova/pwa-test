76439bdc6a3c552547060411fe539198294b8a4e
kaisernova <52025309+kaisernova@users.noreply.github.com>
2022-07-18 16:56:00 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
-window.onload = () => {  
-'use strict';     
-if ('serviceWorker' in navigator) {     
-navigator.serviceWorker  
-.register('./sw.js'); 
-} 
+window.onload = () => {
+    'use strict';
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+        .register('./sw.js');
+    }
 }
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    deferredPrompt = e;
+});
+
+
+const installApp = document.getElementById('installApp');
+
+installApp.addEventListener('click', async () => {
+    if (deferredPrompt !== null) {
+        deferredPrompt.prompt();
+        const { outcome } = await deferredPrompt.userChoice;
+        if (outcome === 'accepted') {
+            deferredPrompt = null;
+        }
+    }
+});
