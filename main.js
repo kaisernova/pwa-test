918e2957bcafbcfb0c8e5277daf0617b4ea19a61
kaisernova <52025309+kaisernova@users.noreply.github.com>
2022-07-26 08:34:06 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,57 @@
+
+
+        function handleConnection() {
+            if (navigator.onLine) {
+                isReachable(getServerUrl()).then(function (online) {
+                    if (online) {
+                        // handle online status
+                        console.log('online');
+                    } else {
+                        console.log('no connectivity');
+                    }
+                });
+            } else {
+                // handle offline status
+                console.log('offline');
+            }
+        }
+
+        function isReachable(url) {
+            /**
+             * Note: fetch() still "succeeds" for 404s on subdirectories,
+             * which is ok when only testing for domain reachability.
+             *
+             * Example:
+             *   https://google.com/noexist does not throw
+             *   https://noexist.com/noexist does throw
+             */
+            return fetch(url, { method: 'HEAD', mode: 'no-cors' })
+                .then(function (resp) {
+                    return resp && (resp.ok || resp.type === 'opaque');
+                })
+                .catch(function (err) {
+                    console.warn('[conn test failure]:', err);
+                });
+        }
+
+        function getServerUrl() {
+            return document.getElementById('serverUrl').value || window.location.origin;
+        }
+
+
+
+
+
 window.onload = () => {
     'use strict';
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker
         .register('./sw.js');
     }
+    console.log("handles");
+    window.addEventListener('online', handleConnection);
+    window.addEventListener('offline', handleConnection);
+    
 }
 
 let deferredPrompt;
