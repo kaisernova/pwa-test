c0e429935a50ea60a37a3b8356b219b431f4e819
test <test@test.com>
2022-08-01 14:56:53 -0500

    test

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-var version = "1.5.1";
-importScripts("https://cdn.subscribers.com/assets/subscribers-sw.js");
 
 function handleConnection() {
 	var titleElement = document.getElementById('status-title');
