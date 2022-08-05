096037732f863cd41b2ec790707e194751e9b5b0
user <user@DESKTOP-KSAA1L2>
2022-08-05 01:29:39 -0500

    cache

--- sw.js
+++ sw.js
@@ -1,29 +1,24 @@
-/*
-Copyright 2015, 2019 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
-
-// Incrementing OFFLINE_VERSION will kick off the install event and force
-// previously cached resources to be updated from the network.
 const OFFLINE_VERSION = 1;
 const CACHE_NAME = 'offline';
-// Customize this with a different URL if needed.
+
 const OFFLINE_URL = 'index.html';
+var filesToCache = [
+'https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.3.2/papaparse.js',
+'/jsstore.worker.min.js',
+'/validador_cedulaJS.js',
+'https://cdn.jsdelivr.net/npm/jsstore/dist/jsstore.min.js',
+'https://cdn.gravitec.net/storage/c4f5c9ab25dcb2e784f72e791b4d1f35/client.js',
+'main.js',
+'/wasmcode/release.js',
+'/wasmcode/release.wasm',
+'/wasmcode/release.wat'
+]; 
 
 self.addEventListener('install', (event) => {
   event.waitUntil((async () => {
     const cache = await caches.open(CACHE_NAME);
-    // Setting {cache: 'reload'} in the new request will ensure that the response
-    // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
     await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
+    await cache.addAll(filesToCache);
   })());
 });
 
