6adf9a1bc14b1d67a0a36970152cd472434977f9
user <user@DESKTOP-KSAA1L2>
2022-08-04 23:47:05 -0500

    ajsute

---
+++ wasmcode/release.d.ts
@@ -0,0 +1,9 @@
+/** Exported memory */
+export declare const memory: WebAssembly.Memory;
+/**
+ * assembly/index/validarCedula
+ * @param digitos `~lib/array/Array<i32>`
+ * @param veces `i32`
+ * @returns `i32`
+ */
+export declare function validarCedula(digitos: Array<number>, veces: number): number;
